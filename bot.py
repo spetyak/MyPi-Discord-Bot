f8a55cff93480a29efb22a5f0e2009f8160908de
spetyak <smpetyak@gmail.com>
2024-04-16 13:57:22 -0400

    added basic commands

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 # I want to be able to:
 # - periodically check temperature/fan status and NOTIFY if necessary (ie. high temps)
@@ -9,13 +10,45 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('MYPI_TOKEN')
 
+
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+# client = discord.Client(intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} connected to server MyPi')
+    print(f'{bot.user} connected to server MyPi')
 
-client.run(TOKEN)
+    # start temp watch routine
+    # notify user if temps rise above threshold and run fan until they cool below
+    # if 5 minutes have passed with no drop to reasonable temps, schdule shutdown and notify user
+
+@bot.command(name='checktemp')
+async def checkTemp(context):
+
+    # return current temp of pi (instantaneous, do not mess with fan)
+    await context.send("Pi current temp: ???")
+
+@bot.command(name='shutdown')
+async def shutdown(context):
+
+    # schedule the pi to shutdown, display shutdown message to user
+    # end bot process (exit()) before shutdown
+    await context.send("Scheduled pi to shutdown: ???")
+
+@bot.command(name='reboot')
+async def reboot(context):
+
+    # schedule the pi to reboot, display reboot message to user
+    await context.send("Scheduled pi to reboot: ???")
+
+@bot.command(name='showactive')
+async def showActive(context):
+
+    # display the current active processes to the user
+    await context.send("Active processes: ???")
+
+bot.run(TOKEN)
